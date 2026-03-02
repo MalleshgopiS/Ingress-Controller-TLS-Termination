ded421be45512d14d5347cf281bdb4fbb17688fb
Mallesh Gopi <malleshgopi@gmail.com>
2026-03-03 00:50:59 +0530

    fix grader

--- solution.sh
+++ solution.sh
@@ -11,18 +11,26 @@ kubectl patch configmap ingress-nginx-config \
   --type merge \
   -p '{"data":{"ssl-session-timeout":"10m"}}'
 
-echo "Deleting existing pod to reload config..."
+echo "Getting current pod..."
 
-kubectl delete pod \
-  -n "$NS" \
-  -l "$APP_LABEL" \
-  --wait=true
+OLD_POD=$(kubectl get pod -n "$NS" -l "$APP_LABEL" \
+  -o jsonpath='{.items[0].metadata.name}')
+
+echo "Deleting pod: $OLD_POD"
+
+kubectl delete pod "$OLD_POD" -n "$NS"
+
+echo "Waiting for new pod to appear..."
+
+sleep 5
+
+NEW_POD=$(kubectl get pod -n "$NS" -l "$APP_LABEL" \
+  -o jsonpath='{.items[0].metadata.name}')
 
 echo "Waiting for new pod Ready..."
 
-kubectl wait pod \
+kubectl wait pod "$NEW_POD" \
   -n "$NS" \
-  -l "$APP_LABEL" \
   --for=condition=Ready \
   --timeout=600s
 
